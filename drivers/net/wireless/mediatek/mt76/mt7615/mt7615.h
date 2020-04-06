76b9444c2ed018e81f18e256a8905ff75f296d93
Sean Wang <sean.wang@mediatek.com>
2020-04-05 21:05:31 -0700

    mt76: mt7615: make mt7615_mcu_send_message generic
    Make mt7615_mcu_send_message as the common code to MMIO and USB based
    devices.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
+++ drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
@@ -474,6 +474,8 @@ void mt7615_mac_reset_work(struct work_struct *work);
 int mt7615_mcu_wait_response(struct mt7615_dev *dev, int cmd, int seq);
 int mt7615_mcu_msg_send(struct mt76_dev *mdev, int cmd, const void *data,
 			int len, bool wait_resp);
+int mt7615_mcu_send_message(struct mt76_dev *mdev, struct sk_buff *skb,
+			    int cmd, bool wait_resp);
 int mt7615_mcu_set_dbdc(struct mt7615_dev *dev);
 int mt7615_mcu_set_eeprom(struct mt7615_dev *dev);
 int mt7615_mcu_set_mac_enable(struct mt7615_dev *dev, int band, bool enable);
