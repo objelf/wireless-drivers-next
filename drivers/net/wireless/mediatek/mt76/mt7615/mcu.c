fangfsz/mt76

mt76: mt7663: add debug message event
add debug message event

Co-developed-by: Sean Wang <sean.wang@mediatek.com>
Signed-off-by: Sean Wang <sean.wang@mediatek.com>

mt7615/mcu.c
@@ -303,6 +303,36 @@ mt7615_mcu_rx_ext_event(struct mt7615_dev *dev, struct sk_buff *skb)
 }
 
 static void
+mt7615_mcu_dbg_msg_event(struct mt7615_dev *dev, struct sk_buff *skb)
+{
+	struct mt7615_mcu_rxd *rxd = (struct mt7615_mcu_rxd *)skb->data;
+	struct mtk_ofl_dbg_msg {
+		__le16 id; /* 0: unknown, 1:print, 2:fwlog */
+		u8 type; /*0: unknown, 1: mem8, 2:mem32, 3:ascii 4:binary */
+		u8 flag;
+		__le32 value;
+		__le16 msg_size;
+		u8 msg[512];
+	} __packed *dbg_msg;
+	int i;
+
+	skb_pull(skb, sizeof(*rxd));
+	dbg_msg = (struct mtk_ofl_dbg_msg *)skb->data;
+
+	dev_err(dev->mt76.dev, "id = 0x%x, type = 0x%x, flag = 0x%x, value = 0x%x, msg_size = %d\n",
+		le16_to_cpu(dbg_msg->id), dbg_msg->type, dbg_msg->flag,
+		le32_to_cpu(dbg_msg->value), le16_to_cpu(dbg_msg->msg_size));
+
+	if (dbg_msg->type == 0x3) {
+		for (i = 0 ; i < le16_to_cpu(dbg_msg->msg_size); i++)
+			if (!dbg_msg->msg[i])
+				dbg_msg->msg[i]=' ';
+
+		dev_err(dev->mt76.dev, "%s\n", dbg_msg->msg);
+	}
+}
+
+static void
 mt7615_mcu_rx_unsolicited_event(struct mt7615_dev *dev, struct sk_buff *skb)
 {
 	struct mt7615_mcu_rxd *rxd = (struct mt7615_mcu_rxd *)skb->data;
@@ -310,6 +340,9 @@ mt7615_mcu_rx_unsolicited_event(struct mt7615_dev *dev, struct sk_buff *skb)
 	switch (rxd->eid) {
 	case MCU_EVENT_EXT:
 		mt7615_mcu_rx_ext_event(dev, skb);
+		break;
+	case MCU_EVENT_DBG_MSG:
+		mt7615_mcu_dbg_msg_event(dev, skb);
 		break;
 	default:
 		break;
@@ -325,6 +358,7 @@ void mt7615_mcu_rx_event(struct mt7615_dev *dev, struct sk_buff *skb)
 	    rxd->ext_eid == MCU_EXT_EVENT_FW_LOG_2_HOST ||
 	    rxd->ext_eid == MCU_EXT_EVENT_ASSERT_DUMP ||
 	    rxd->ext_eid == MCU_EXT_EVENT_PS_SYNC ||
+	    rxd->eid == MCU_EVENT_DBG_MSG ||
 	    !rxd->seq)
 		mt7615_mcu_rx_unsolicited_event(dev, skb);
 	else

objelf/wireless-drivers-next

drivers/net/wireless/mediatek/mt76/mt7615/mcu.c
@@ -375,6 +375,43 @@ mt7615_mcu_bss_event(struct mt7615_dev *dev, struct sk_buff *skb)
 }
 
 static void
+mt7615_mcu_debug_msg_event(struct mt7615_dev *dev, struct sk_buff *skb)
+{
+	struct mt7615_mcu_rxd *rxd = (struct mt7615_mcu_rxd *)skb->data;
+	struct mtk_ofl_dbg_msg {
+		__le16 id; /* 0: unknown, 1:print, 2:fwlog */
+		u8 type; /*0: unknown, 1: mem8, 2:mem32, 3:ascii 4:binary */
+		u8 flag;
+		__le32 value;
+		__le16 msg_size;
+		u8 msg[512];
+	} __packed *dbg_msg;
+	int i;
+
+	skb_pull(skb, sizeof(*rxd));
+	dbg_msg = (struct mtk_ofl_dbg_msg *)skb->data;
+
+	dev_err(dev->mt76.dev, "id = 0x%x, type = 0x%x, flag = 0x%x, value = 0x%x, msg_size = %d\n",
+		le16_to_cpu(dbg_msg->id), dbg_msg->type, dbg_msg->flag,
+		le32_to_cpu(dbg_msg->value), le16_to_cpu(dbg_msg->msg_size));
+
+	if (dbg_msg->type == 0x3) {
+		for (i = 0 ; i < le16_to_cpu(dbg_msg->msg_size); i++)
+			if (!dbg_msg->msg[i])
+				dbg_msg->msg[i]=' ';
+
+		dev_err(dev->mt76.dev, "%s\n", dbg_msg->msg);
+	}
+
+	/*
+	print_hex_dump(KERN_ERR, "fwlog", DUMP_PREFIX_OFFSET, 32, 8,
+		       dbg_msg->msg, dbg_msg->msg_size, true);
+	*/
+
+	/* TODO: Integrate the event to debugfs/ftrace */
+}
+
+static void
 mt7615_mcu_rx_unsolicited_event(struct mt7615_dev *dev, struct sk_buff *skb)
 {
 	struct mt7615_mcu_rxd *rxd = (struct mt7615_mcu_rxd *)skb->data;
@@ -389,6 +426,9 @@ mt7615_mcu_rx_unsolicited_event(struct mt7615_dev *dev, struct sk_buff *skb)
 		return;
 	case MCU_EVENT_BSS_ABSENCE:
 		mt7615_mcu_bss_event(dev, skb);
+		break;
+	case MCU_EVENT_DBG_MSG:
+		mt7615_mcu_debug_msg_event(dev, skb);
 		break;
 	default:
 		break;
@@ -407,6 +447,7 @@ void mt7615_mcu_rx_event(struct mt7615_dev *dev, struct sk_buff *skb)
 	    rxd->eid == MCU_EVENT_SCHED_SCAN_DONE ||
 	    rxd->eid == MCU_EVENT_BSS_ABSENCE ||
 	    rxd->eid == MCU_EVENT_SCAN_DONE ||
+	    rxd->eid == MCU_EVENT_DBG_MSG ||
 	    !rxd->seq)
 		mt7615_mcu_rx_unsolicited_event(dev, skb);
 	else