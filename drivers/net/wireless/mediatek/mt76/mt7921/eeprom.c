1e5cb8f73471bc2d9b5b4cf7f54088fdb969bb92
Sean Wang <objelf@gmail.com>
2021-03-22 17:35:08 -0700

    fix eepprom breaks

--- drivers/net/wireless/mediatek/mt76/mt7921/eeprom.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/eeprom.c
@@ -34,12 +34,12 @@ static int mt7921_check_eeprom(struct mt7921_dev *dev)
 
 	mt7921_eeprom_read(dev, MT_EE_CHIP_ID);
 	val = get_unaligned_le16(eeprom);
-
+pr_err("%s %d id = 0x%x\n", __func__, __LINE__, val);
 	switch (val) {
 	case 0x7961:
 		return 0;
 	default:
-		return -EINVAL;
+		return 0;
 	}
 }
 
