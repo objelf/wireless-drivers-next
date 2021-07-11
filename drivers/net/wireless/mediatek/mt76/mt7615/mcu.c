4b2248d81ff56607c67c75fe330b5f98e67a6e46
Sean Wang <objelf@gmail.com>
2021-07-10 23:54:32 -0700

    mt76: mt7615: drop mt7615_mcu_reg_[rr, wr]
    drop mt7615_mcu_reg_[rr, wr] because we already moved to mt76_connac_mcu
    library.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7615/mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/mcu.c
@@ -2783,31 +2783,3 @@ int mt7615_mcu_set_p2p_oppps(struct ieee80211_hw *hw,
 	return mt76_mcu_send_msg(&dev->mt76, MCU_CMD_SET_P2P_OPPPS, &req,
 				 sizeof(req), false);
 }
-
-u32 mt7615_mcu_reg_rr(struct mt76_dev *dev, u32 offset)
-{
-	struct {
-		__le32 addr;
-		__le32 val;
-	} __packed req = {
-		.addr = cpu_to_le32(offset),
-	};
-
-	return mt76_mcu_send_msg(dev, MCU_CMD_REG_READ, &req, sizeof(req),
-				 true);
-}
-EXPORT_SYMBOL_GPL(mt7615_mcu_reg_rr);
-
-void mt7615_mcu_reg_wr(struct mt76_dev *dev, u32 offset, u32 val)
-{
-	struct {
-		__le32 addr;
-		__le32 val;
-	} __packed req = {
-		.addr = cpu_to_le32(offset),
-		.val = cpu_to_le32(val),
-	};
-
-	mt76_mcu_send_msg(dev, MCU_CMD_REG_WRITE, &req, sizeof(req), false);
-}
-EXPORT_SYMBOL_GPL(mt7615_mcu_reg_wr);
