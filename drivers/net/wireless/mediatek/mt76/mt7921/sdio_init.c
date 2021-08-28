19d9925451fe85b5cc72bb205ceafe65d130158c
Sean Wang <sean.wang@mediatek.com>
2021-08-27 17:19:27 -0700

    mt76: mt7921: introduce mt7921s support
    Introduce support for mt7921s 802.11ax (Wi-Fi 6) 2x2:2SS chipset.
    
    Tested-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

---
+++ drivers/net/wireless/mediatek/mt76/mt7921/sdio_init.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: ISC
+/* Copyright (C) 2021 MediaTek Inc. */
+
+#include <linux/etherdevice.h>
+#include "mt7921.h"
+#include "mac.h"
+#include "mcu.h"
+#include "eeprom.h"
+
+void mt7921s_unregister_device(struct mt7921_dev *dev)
+{
+	struct mt76_connac_pm *pm = &dev->pm;
+
+	mt76_unregister_device(&dev->mt76);
+	cancel_delayed_work_sync(&pm->ps_work);
+	cancel_work_sync(&pm->wake_work);
+
+	mt76s_deinit(&dev->mt76);
+	mt7921_mcu_exit(dev);
+
+	mt76_free_device(&dev->mt76);
+}
