d6b64c8bb5d61806ce8f6de5c16953e7157ef489
Sean Wang <objelf@gmail.com>
2022-11-20 22:30:41 -0800

    Revert "Bluetooth: btmtk: introduce btmtk reset work"
    This reverts commit 73512291597b17a2ccbc770333cadc41bc4bca54.

--- drivers/bluetooth/btmtk.h
+++ drivers/bluetooth/btmtk.h
@@ -119,11 +119,6 @@ struct btmtk_hci_wmt_params {
 	u32 *status;
 };
 
-struct btmtk_reset_work {
-	struct hci_dev *hdev;
-	struct work_struct work;
-};
-
 typedef int (*wmt_cmd_sync_func_t)(struct hci_dev *,
 				   struct btmtk_hci_wmt_params *);
 
@@ -136,8 +131,6 @@ int btmtk_setup_firmware_79xx(struct hci_dev *hdev, const char *fwname,
 
 int btmtk_setup_firmware(struct hci_dev *hdev, const char *fwname,
 			 wmt_cmd_sync_func_t wmt_cmd_sync);
-void btmtk_init_reset_work(struct hci_dev *hdev, work_func_t func);
-void btmtk_reset_sync(struct hci_dev *hdev);
 #else
 
 static inline int btmtk_set_bdaddr(struct hci_dev *hdev,
@@ -158,11 +151,4 @@ static int btmtk_setup_firmware(struct hci_dev *hdev, const char *fwname,
 	return -EOPNOTSUPP;
 }
 
-static void btmtk_init_reset_work(struct hci_dev *hdev, work_func_t func)
-{
-}
-
-static void btmtk_reset_sync(struct hci_dev *hdev)
-{
-}
 #endif
